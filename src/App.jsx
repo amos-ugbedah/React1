8280d35fd689f28744fea41cb0338c732d47e23b
Ugbedah Amos <ugbedahamos@gmail.com>
2025-03-04 14:40:15 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-} from "react-router-dom";
-import MainLayout from "./layouts/MainLayout";
-import HomePage from "./pages/HomePage";
-import JobsPage from "./pages/JobsPage";
-import NotFoundPage from './pages/NotFoundPage';
-import JobPage from "./pages/JobPage";
-
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage'; // Ensure the path and name are correct
+import JobPage from './pages/JobPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
+    <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
-      <Route path="jobs" element={<JobsPage />} />
-      <Route path="jobs/:id" element={<JobPage />} />
-      <Route path="*" element={<NotFoundPage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} />
+      <Route path='*' element={<NotFoundPage />} /> {/* Corrected component name */}
     </Route>
   )
 );
